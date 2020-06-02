24da5911eb7db577c0075fcb989c0b9614d44ab5
Alex-kku <leha.kushpelev@mai.ru>
2020-06-02 22:34:34 +0300

    Add comments

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,8 +1,8 @@
-#include <iosrteam>
+#include <iosrteam> 
 int main()
 {
     std::string name;
-    getline(cin, name);
-    std::cout << "Hello world from " << name << std::endl;
+    getline(cin, name);//enter the name
+    std::cout << "Hello world from " << name << std::endl;//print
     return 0;
-}
+} 
